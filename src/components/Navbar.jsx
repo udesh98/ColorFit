7a8c016cc13a238380c72a63f348bfbe28d7283a
Udesh.Maduranga <udesh.maduranga@cube.global>
2025-05-22 14:09:25 +0530

    update navigates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     const toggleMenu = () => setMenuOpen(!menuOpen); 
 
     const handleLogoClick = () => {
-        navigate("/");
+        navigate("/ColorFit");
     
         // Remove hash from URL and scroll to top
         setTimeout(() => {
