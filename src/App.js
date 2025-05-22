9d5303983832b6f38b9ebb5f387a090bfa10c848
Udesh.Maduranga <udesh.maduranga@cube.global>
2025-05-22 14:35:42 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import MainFeature from "./components/MainFeature"; // Replace with your actual main component path
 import NotFoundPage from "./components/NotFoundPage";
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/main" element={<MainFeature />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
